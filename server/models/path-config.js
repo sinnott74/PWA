69734d8ced280c9f39156f1ad8d7ba10e699e504
Daniel Sinnott <danielsi@ie.ibm.com>
2016-11-11 11:18:50 +0000

    removed google font & moved font to a variable sass file

--- server/models/path-config.js
+++ server/models/path-config.js
@@ -6,32 +6,28 @@ var pathConfigs = {
     view: 'index',
     title: 'Index',
     inlineStyles: getFileContents(['/styles/core.css']),
-    remoteStyles: ['https://fonts.googleapis.com/css?family=Roboto:' +
-      '400,300,700,500,400italic'],
+    remoteStyles: [],
     remoteScripts: ['/scripts/static-page.js']
   },
   '/url-1': {
     view: 'url-1',
     title: 'URL 1',
     inlineStyles: getFileContents(['/styles/core.css']),
-    remoteStyles: ['https://fonts.googleapis.com/css?family=Roboto:' +
-      '400,300,700,500,400italic'],
+    remoteStyles: [],
     remoteScripts: ['/scripts/static-page.js']
   },
   '/url-2': {
     view: 'url-2',
     title: 'URL 2',
     inlineStyles: getFileContents(['/styles/core.css']),
-    remoteStyles: ['https://fonts.googleapis.com/css?family=Roboto:' +
-      '400,300,700,500,400italic'],
+    remoteStyles: [],
     remoteScripts: ['/scripts/static-page.js']
   },
   '/app-shell': {
     view: '',
     title: 'App Shell',
     inlineStyles: getFileContents(['/styles/core.css']),
-    remoteStyles: ['https://fonts.googleapis.com/css?family=Roboto:' +
-      '400,300,700,500,400italic'],
+    remoteStyles: [],
     remoteScripts: ['/scripts/core.js']
   }
 };
