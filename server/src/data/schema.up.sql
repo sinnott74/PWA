7d1cff32fa65ff9ce1efed14d2920c77d45f63d5
Daniel Sinnott <danielsi@ie.ibm.com>
2017-02-19 00:48:44 +0000

    removing unneeded sql files

--- server/src/data/schema.up.sql
+++
@@ -1,6 +0,0 @@
--- serial means auto increment
-CREATE TABLE users (
-  userid serial primary key,
-  username varchar(60),
-  password varchar(60)
-);
--- server/src/data/seed.sql
+++
@@ -1,2 +0,0 @@
--- default uses the default value which is this case is to auto increment
-INSERT INTO users VALUES (default, 'sinnott74@hotmail.com', 'password');
