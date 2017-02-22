2850b23234c1df7ef5468824fdbb3432ed2cc9a3
Daniel Sinnott <danielsi@ie.ibm.com>
2017-02-22 16:01:39 +0000

    linting errors

--- server/src/routers/apiRouter.js
+++ server/src/routers/apiRouter.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var router = express.Router();
+var router = new express.Router();
 var APIController = require('../controllers/api-controller');
 
 router.get('/*', function(req, res) {
--- server/src/routers/staticPageRouter.js
+++ server/src/routers/staticPageRouter.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var router = express.Router();
+var router = new express.Router();
 var PageController = require('../controllers/static-page-controller');
 
 router.get('/*', function(req, res) {
