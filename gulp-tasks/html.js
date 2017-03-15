734241d8971d31af7ef517cd52aa83e75be8cee5
Daniel Sinnott <danielsi@ie.ibm.com>
2017-03-15 14:57:05 +0000

    fixing @version@ replacement in sidenav

--- gulp-tasks/html.js
+++ gulp-tasks/html.js
@@ -7,13 +7,21 @@ gulp.task('html:watch', function() {
   gulp.watch(GLOBAL.config.src + '/**/*.html', ['vulcanize', 'html']);
 });
 
+gulp.task('html', ['vulcanize', 'html:copyToDest', 'html:replaceVersionNo']);
+
 // copies all html files into the dist directory
 // ignoring webcomponent elements as these are included in elements.html during vulcanization
-gulp.task('html', function() {
+gulp.task('html:copyToDest', function() {
   return gulp.src([
     GLOBAL.config.src + '/**/*.html', '!/**/elements/**/*.html'
   ])
     .pipe(gulpif(GLOBAL.config.env === 'prod', minifyHtml()))
+    .pipe(gulp.dest(GLOBAL.config.dest));
+});
+
+gulp.task('html:replaceVersionNo', ['vulcanize', 'html:copyToDest'], function() {
+  return gulp.src([GLOBAL.config.dest + '/**/*.html'])
     .pipe(replace(/@VERSION@/g, GLOBAL.config.version))
     .pipe(gulp.dest(GLOBAL.config.dest));
 });
+
