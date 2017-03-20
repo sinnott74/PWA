026c08207cbede8a78c9dfeb0a02a1fb996c4601
Daniel Sinnott <danielsi@ie.ibm.com>
2017-03-20 17:24:14 +0000

    Added a second user to seed data

--- server/src/data/seeds/users.js
+++ server/src/data/seeds/users.js
@@ -8,7 +8,15 @@ exports.seed = function(knex, Promise) {
       password: '$2a$10$qO8jwX8MNM2B9VB0gYjsL.XL5JRSG4T9990qgcLm8xo6Ot1O6yX5u',
       firstname: 'Daniel',
       lastname: 'Sinnott',
-      dob: new Date()}
+      dob: new Date()
+    },
+    {
+      username: 'test@test.com',
+      password: '$2a$10$qO8jwX8MNM2B9VB0gYjsL.XL5JRSG4T9990qgcLm8xo6Ot1O6yX5u',
+      firstname: 'Test',
+      lastname: 'Test',
+      dob: new Date()
+    }
   ];
 
 
