98fbee598f337724e30155b0cc69d6673a9a67d9
Daniel Sinnott <danielsi@ie.ibm.com>
2017-03-20 17:32:18 +0000

    calling business logic from routers

--- server/src/routers/apiRouter.js
+++ server/src/routers/apiRouter.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = new express.Router();
 var APIController = require('../controllers/api-controller');
+var businessLogic = require('../facades/businessLogicMiddleware');
 
 var apiController = new APIController();
+
+router.all('/*', businessLogic);
 
 router.all('/*', function(req, res) {
   apiController.onRequest(req, res);
--- server/src/routers/staticPageRouter.js
+++ server/src/routers/staticPageRouter.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = new express.Router();
 var PageController = require('../controllers/static-page-controller');
+var businessLogic = require('../facades/businessLogicMiddleware');
 
 var pageController = new PageController();
+
+router.all('/*', businessLogic);
 
 router.all('/*', function(req, res) {
   pageController.onRequest(req, res);
