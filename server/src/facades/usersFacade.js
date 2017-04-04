a5ff8de2223d00ec912cd3da3e3b57fbf08d8a66
Daniel Sinnott <danielsi@ie.ibm.com>
2017-04-04 15:56:04 +0100

    Updated facade to seperate business layer & added read by ID operation

--- server/src/facades/usersFacade.js
+++ server/src/facades/usersFacade.js
@@ -1,26 +1,26 @@
 'user strict';
 
-var express = require('express');
-var router = new express.Router();
-
 var userDAO = require('../DAO/usersDAO');
 
-router.get('/', (req, res, next) => {
+// router.get('/:id', (req, res, next) => {
+//   console.log('In user get - ID: ' + req.params.id);
+//   userDAO.read(req.params.id)
+//   .then((user) => {
+//     res.locals.model = user;
+//     next();
+//   });
+// });
+
+var userFacade = {};
+
+userFacade.readByID = function(userKey) {
+  console.log('Read user by ID - ' + userKey.id);
+  return userDAO.read(userKey.id);
+};
+
+userFacade.listAllUsers = function() {
   console.log('List all users');
-  userDAO.list()
-  .then((users) => {
-    res.locals.model = users;
-    next();
-  });
-});
+  return userDAO.list();
+};
 
-router.get('/:id', (req, res, next) => {
-  console.log('In user get - ID: ' + req.params.id);
-  userDAO.read(req.params.id)
-  .then((user) => {
-    res.locals.model = user;
-    next();
-  });
-});
-
-module.exports = router;
+module.exports = userFacade;
