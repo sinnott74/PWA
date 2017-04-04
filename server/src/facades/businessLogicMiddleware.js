020cec40ba1c50f049327b8c815175f23b0127da
Daniel Sinnott <danielsi@ie.ibm.com>
2017-04-04 15:53:43 +0100

    Moved busines logic middleware

--- server/src/facades/businessLogicMiddleware.js
+++
@@ -1,9 +0,0 @@
-'user strict';
-
-var express = require('express');
-var userFacade = require('../facades/usersFacade');
-var router = new express.Router();
-
-router.use('/users', userFacade);
-
-module.exports = router;
