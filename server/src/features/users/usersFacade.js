03ec508e86f814d1cc27b04b71777d2a863bc51c
Daniel Sinnott <danielsi@ie.ibm.com>
2017-04-06 11:46:48 +0100

    removed commeted out method

--- server/src/features/users/usersFacade.js
+++ server/src/features/users/usersFacade.js
@@ -2,15 +2,6 @@
 
 var userDAO = require('./usersDAO');
 
-// router.get('/:id', (req, res, next) => {
-//   console.log('In user get - ID: ' + req.params.id);
-//   userDAO.read(req.params.id)
-//   .then((user) => {
-//     res.locals.model = user;
-//     next();
-//   });
-// });
-
 var userFacade = {};
 
 userFacade.readByID = function(userKey) {
