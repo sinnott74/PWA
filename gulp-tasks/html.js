cd5ed08d83882df78af6fbc171eb9a60085385ed
Daniel Sinnott <danielsi@ie.ibm.com>
2017-04-12 20:41:23 +0100

    moving all element / html build tasks to single file

--- gulp-tasks/html.js
+++ gulp-tasks/html.js
@@ -2,12 +2,22 @@ var gulp = require('gulp');
 var gulpif = require('gulp-if');
 var minifyHtml = require('gulp-minify-html');
 var replace = require('gulp-replace');
+var vulcanize = require('gulp-vulcanize');
+var polylint = require('gulp-polylint');
+var runSequence = require('run-sequence');
 
 gulp.task('html:watch', function() {
-  gulp.watch(GLOBAL.config.src + '/**/*.html', ['elements', 'html']);
+  gulp.watch(GLOBAL.config.src + '/**/*.html', ['html']);
 });
 
-gulp.task('html', ['elements', 'html:copyToDest', 'html:replaceVersionNo']);
+gulp.task('html', ['bower'], function(cb) {
+  runSequence(
+    'polylint',
+    'vulcanize',
+    'html:replaceVersionNo',
+    cb
+  );
+});
 
 // copies all html files into the dist directory
 // ignoring webcomponent elements as these are included in elements.html during vulcanization
@@ -19,9 +29,28 @@ gulp.task('html:copyToDest', function() {
     .pipe(gulp.dest(GLOBAL.config.dest));
 });
 
-gulp.task('html:replaceVersionNo', ['vulcanize', 'html:copyToDest'], function() {
+gulp.task('html:replaceVersionNo', function() {
   return gulp.src([GLOBAL.config.dest + '/**/*.html'])
     .pipe(replace(/@VERSION@/g, GLOBAL.config.version))
     .pipe(gulp.dest(GLOBAL.config.dest));
+});
+
+gulp.task('polylint', function() {
+  return gulp.src(GLOBAL.config.src + '/elements/**/*.html')
+    .pipe(polylint({noRecursion: true}))
+    .pipe(polylint.reporter(polylint.reporter.stylishlike))
+    .pipe(polylint.reporter.fail({buffer: true, ignoreWarnings: false}));
+});
+
+// Vulcanize take all of our polymer elements and
+//  merges them into a single file so they can be retrieved in a single request
+gulp.task('vulcanize', function() {
+  return gulp.src(GLOBAL.config.src + '/elements/elements.html')
+    .pipe(vulcanize({
+      stripComments: true,
+      inlineScripts: true,
+      inlineCss: true
+    }))
+    .pipe(gulp.dest(GLOBAL.config.dest + '/elements'));
 });
 
