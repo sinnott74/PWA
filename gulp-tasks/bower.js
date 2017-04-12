22491a4d6578f11413c8426ef0d9c13aa16cb2b5
Daniel Sinnott <danielsi@ie.ibm.com>
2017-04-12 20:41:56 +0100

    removing not used require

--- gulp-tasks/bower.js
+++ gulp-tasks/bower.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 var bower = require('gulp-bower');
-var path = require('path');
 
 // installs bower dependencies & pipes them into dist/lib
 gulp.task('bower', function(cb) {
