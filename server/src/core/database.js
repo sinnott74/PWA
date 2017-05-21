4c2b13766ab2f6c59778f6686ca9e06ec9a0778a
Daniel Sinnott <danielsi@ie.ibm.com>
2017-05-21 21:38:18 +0100

    Tidying console logs

--- server/src/core/database.js
+++ server/src/core/database.js
@@ -13,9 +13,8 @@ var knexConfig = require('../data/knexfile.js'); // require knex configuration o
 var mode = process.env.NODE_ENV || 'development';
 var dbConfig = knexConfig[mode];
 
-console.log('Database mode used: ' + mode);
-console.log('dbConfig used:');
-console.log(dbConfig.connection);
+console.log('Database mode used:', mode);
+console.log('dbConfig used:', dbConfig.connection);
 var knex = require('knex')(dbConfig);
 
 module.exports = knex;
