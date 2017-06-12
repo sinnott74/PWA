6114815e5f6e759373b7484a66f0410242d59a59
Daniel Sinnott <danielsi@ie.ibm.com>
2017-06-12 10:43:49 +0100

    Also delete server build dir

--- gulp-tasks/clean.js
+++ gulp-tasks/clean.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var del = require('del');
 
 gulp.task('clean', function(cb) {
-  del([global.config.clientDest], {dot: true})
+  del([global.config.clientDest, global.config.serverBuild], {dot: true})
     .then(function() {
       cb();
     });
